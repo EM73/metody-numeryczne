18c2f6791b7a318a0c712f8ea30bc27993153204
Marceli Toton <marcel.toton@gmail.com>
2019-03-20 23:17:08 +0100

    lista 2

---
+++ I.py
@@ -0,0 +1,35 @@
+Witam na kursie,
+w semestrze mamy do dyspozycji 14/15 laboratoriów (w zależności od dnia odbywania się zajęć). Ostatnie laboratoria rezerwuję zawsze na wpisanie ocen oraz rozstrzygnięcie przypadków skrajnych. Pierwszych 10 laboratoriów będzie poświęconych na realizację 5 list zadań z pythona o narastającym stopniu trudności, na każdą listę macie max 2 laboratoria, co daje Wam dużą elastyczność. Przykładowo jeśli ktoś na pierwszych zajęciach zdąży zrobić pierwszą i drugą listę to ma zaliczone łącznie 4 zajęcia. Maksymalnie po 10 laboratoriach zaczynacie pracę nad projektem (grupy 3-4 osobowe), projekt będzie omówiony w dalszej części semestru. Można zgłaszać własne pomysły.
+
+Zajęcia jako same nie są obowiązkowe, obowiązkowe jest terminowe lub przedterminowe osobiste oddanie zadań. Podczas oddania zadań często proszę o modyfikację zadania tak, by zweryfikować czy było wykonane samodzielnie lub chociaż czy student/ka w pełni rozumie zagadnienie. Oddanie cudzego rozwiązania traktuję gorzej niż nie oddanie niczego. Do oddania listy podchodzi się raz - nie ma możliwości poprawy na innych zajęciach. Na ocenę zadania znaczący wpływ ma to co mówicie, jak tłumaczycie swoje rozwiązanie oraz czy potraficie wprowadzić zmiany do programu. Zupełnie normalnym jest, że dwoje studentów oddających "takie samo" zadanie może więc otrzymać dwie, nawet skrajnie różne oceny.
+
+Treści zadań i cały kurs jest publicznie dostępny na Github:
+https://github.com/realgs/python-course.git
+
+Można ściągnąć ZIP (bardzo odradzam), lub ściągnąć za pomocą polecenia git clone https://github.com/realgs/python-course.git . Żeby to zrobić, trzeba mieć zainstalowanego gita. Gdyby coś nie szło - nie bójcie się pytać, po to są zajęcia żeby Wam pomóc i nauczyć czegoś pożytecznego. Pamiętajcie też, żeby najpierw spróbować poczytać na Google - jakikolwiek problem aktualnie macie. Na 99,9% wielu ludzi miało ten sam problem i jest już rozwiązany i często jest w top5 wyników wyszukiwania. Przykładowo tu możecie zadać frazę: "how to download a git repository".
+
+Zadania na kurs przygotowałem po angielsku, dla Waszego dobra. W naukach technicznych przytłaczająca większość źródeł jest w tym języku, stackoverflow, na którym znajdziecie wiele rozwiązań waszych problemów też. Mając treść zadania po angielsku będzie Wam łatwiej wyszukać np. co to jest "ternary operator" niż "operator 3-arny". Poza tym niektóre języki programowania wręcz przypominają pisanie po angielsku (range, print, for, ...). Nie ma dobrego programisty, który nie rozumie tego języka. Jeśli u kogoś j. angielski stanowiłby przeszkodę nie do pokonania - bardzo chętnie i z cierpliwością pomogę.
+
+
+konsultacje:
+czwartek 20:30 410 B-1
+
+punktacja:
+listy 1-5 łącznie 40pkt
+projekt 15pkt
+----------------------
+razem 55pkt
+
+oceny:
+55pkt : 5.5
+50-54 : 5.0
+45-49 : 4.5
+40-44 : 4.0
+35-39 : 3.5
+30-34 : 3.0
+< 30 : 2.0
+
+Literatura:
+Robert Johansson, Numerical Python
+Javier Riverola Gurruchaga, Python Recipes for Engineers and Scientists
+Jaan Kiusalaas, Numerical Methods in Engineering with Python 3
